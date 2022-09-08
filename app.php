9a634bdb97af247e8f0cef4c0b53688d8fcadffc
daltamara <112918330+daltamara@users.noreply.github.com>
2022-09-08 16:34:27 -0500

    Add files via upload

--- app.php
+++ app.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    
     <title> Tracker APP</title>
+    <link rel="stylesheet" type="text/css" href="map.css">
 </head>
 
 <?php   
@@ -33,10 +36,12 @@ $arr=json_decode(ob_get_clean());
         <h3>Date/Hour : <span id="Date"><?php echo $arr->{"Date"}?></span></h3>
     </div>
 
+    <div id="map"></div>
+
 <script src="/TheTrackerApp/data.js">
 </script>
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHDHGpcsDEOlqBbnL1BkZlL2Tfpu8XVuM&callback=iniciarMap">
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCHDHGpcsDEOlqBbnL1BkZlL2Tfpu8XVuM&callback=iniciarMap">    
 </script>
     
 </body>
