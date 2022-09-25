35eea58b1a131840288fec5dff04df03b59b5c67
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-25 18:14:04 -0500

    map into historicos

--- historicos.php
+++ historicos.php
@@ -5,6 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+        integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+        crossorigin=""/>
+
+        <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
+        integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
+        crossorigin=""></script>
+
         <title> Historicos </title>
     </head>
 
@@ -34,6 +42,12 @@
             value="2017-06-01T08:30" />
 
             <a href="index.php"> main page </a>
+
+        <div>
+
+        <span id="start"></span>
+
+        </div>
 
         <div id="map" style="height: 500px">
         </div>
