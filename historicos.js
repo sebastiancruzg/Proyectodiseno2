bea362aeaddbb7e104e96c237174599d8180ab7e
telematica <telematica@109819-lab-o.uninorte.local>
2022-09-26 15:57:25 -0500

    js

--- historicos.js
+++ historicos.js
@@ -4,8 +4,13 @@ form.addEventListener('submit',function(e){
     e.preventDefault();
     console.log('click')
     var datos= new FormData(form);
+
+    $.post(post.php, datos);
     
     console.log(datos.get('start'))
     console.log(datos.get('end'))
 
+
+
 })
+passVal();
