4ab46980b740b431790d6915064acd01c58adcfd
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-26 09:26:20 -0500

    date format

--- post.php
+++ post.php
@@ -1,7 +1,26 @@
 <?php
 
+
 $startphp=$_POST['start'];
 $endphp=$_POST['end'];
+
+date_format($startphp, 'Y-m-d H:i:s');
+echo $startphp;
+
+$servername = "database-1.cgvkojo0ntbn.us-east-1.rds.amazonaws.com";
+$username = "std";
+$password = "tetris72";
+$dbname = "firts";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+  
+$result = $conn->query($sql);
 
 
 ?>
