5f6ab50464c24ddd7a6abcd62545fc2471c412b5
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-27 00:59:47 -0500

    js5

--- historicos.js
+++ historicos.js
@@ -2,16 +2,16 @@ function getInfo(){
 
     var star= document.getElementById('start').value;
     var en= document.getElementById('start').value;
-    var start=star.replace('T','');
-    var end=en.replace('T','');
+    var start=star.replace('T',' ');
+    var end=en.replace('T',' ');
 
 $.getJSON('post.php', {var1: "start", var2: "start"}, function (data, textStatus, jqXHR) {
     console.log(data);
     var lati = data.map(a => a.latitude);
     var long = data.map(a => a.longitude);
 
-    latgot = latgot.map(e => parseFloat(e));
-    longot = longot.map(e => parseFloat(e))
+    lati = lati.map(e => parseFloat(e));
+    long = long.map(e => parseFloat(e))
 
     console.log(lati);
     console.log(long);
