4bd35ef7eca9a167a2df772107f4ae1a6bcca427
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-27 00:56:04 -0500

    js3

--- historicos.js
+++ historicos.js
@@ -6,7 +6,7 @@ function getInfo(){
     var end=en.replace('T','');
 
 $.getJSON('post.php', {var1: "start", var2: "start"}, function (data, textStatus, jqXHR) {
-    
+    console.log(data);
     var lati = data.map(a => a.latitude);
     var long = data.map(a => a.longitude);
 
