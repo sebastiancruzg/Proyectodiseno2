d1d1555bc77cfd89fdb9f8a2f4f21bb68a752fda
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-27 00:14:54 -0500

    null7

--- post.php
+++ post.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   $row = $result->fetch_assoc();
-  echo json_encode($result);
+  echo json_encode($row);
   } else {
     echo "0 results";
   }
