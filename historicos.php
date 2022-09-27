17e45c5e9e065b121e2fae624ca8e11a3f0bce72
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-26 23:53:17 -0500

    onclick4

--- historicos.php
+++ historicos.php
@@ -8,6 +8,8 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
         <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
+        <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+
         <title> Historicos </title>
 </head>
 
