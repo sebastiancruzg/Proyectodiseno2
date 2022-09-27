d601baa5005bc4c493eb6db824164aab3d21c1f1
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-27 00:09:48 -0500

    null

--- post.php
+++ post.php
@@ -12,16 +12,23 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM  tabla ORDER  BY Datemy DESC LIMIT  1"; 
+$sql = "SELECT * FROM  tabla ORDER  BY Datemy DESC LIMIT  1";  
 $result = $conn->query($sql);
+
 
 if ($result->num_rows > 0) {
     // output data of each row
-$row = $result->fetch_assoc();
+  $row = $result->fetch_assoc();
 
-echo json_encode($arr);
-
-$conn->close();
-}
+  $arr = array(
+    'latitude' => $row["Latitude"],
+    'longitude' => $row["Longitude"],
+    'date' => $row["Datemy"]
+  );
+  echo json_encode($arr);
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
 
 ?>
