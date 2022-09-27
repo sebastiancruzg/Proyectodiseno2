fe5d51ab46932fccc292654b8f5d5619e2a9486c
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-27 00:04:33 -0500

    queryagain3

--- post.php
+++ post.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Latitude, Longitude FROM tabla ";  
+$sql = "SELECT * FROM  tabla ORDER  BY Datemy DESC LIMIT  1"; 
 $result = $conn->query($sql);
 
 exit(json_encode($result));
