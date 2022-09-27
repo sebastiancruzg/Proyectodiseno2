3da0c4840e14c47ff818a74ddef325cd7636ded3
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-09-26 22:46:40 -0500

    querynew2

--- post.php
+++ post.php
@@ -12,8 +12,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Latitude, Longitude FROM tabla WHERE Datemy >= '2022-09-25 00:37:24' 
-AND Datemy <= '2022-09-25 00:39:45' ";  
+$sql = "SELECT Latitude, Longitude FROM tabla WHERE Datemy >= '2022-09-25 00:37' 
+AND Datemy <= '2022-09-25 00:39' ";  
 $result = $conn->query($sql);
 
 exit(json_encode($result));
