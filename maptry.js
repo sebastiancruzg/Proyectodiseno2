522328e95626e8a9aff4f0d6fb09c903435c84a5
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-10-01 15:50:47 -0500

    radius

--- maptry.js
+++ maptry.js
@@ -10,15 +10,21 @@ L.tileLayer(tilesProvider,{
 
 var marker=L.marker([0,0]);
 
+
 Map.on('click', function(e) {
+    var radius = e.accuracy / 2;
 
     marker.remove(Map);
 
     marker.setLatLng(e.latlng).addTo(Map);
+    L.circle(e.latlng, radius,
+        {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.5,
+          radius: 400
+        }).addTo(map);
     console.log(e.latlng);
 
-    
-
 });
-
 
