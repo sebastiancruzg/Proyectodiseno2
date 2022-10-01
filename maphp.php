5e68169b82a9b844d93383e3bce8acbcd53c30db
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-10-01 18:50:20 -0500

    query

--- maphp.php
+++ maphp.php
@@ -1,7 +1,8 @@
 <?php   
 
-$startphp = $_GET['var1'];
-$endphp = $_GET['var2'];
+$latphp = $_GET['var1'];
+$lngphp = $_GET['var2'];
+
 $servername = "database-1.cgvkojo0ntbn.us-east-1.rds.amazonaws.com";
 $username = "std";
 $password = "tetris72";
@@ -14,13 +15,12 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = mysqli_query($conn,"SELECT Datemy, 
-( 6371 * acos(cos(radians('$startphp')) * cos(radians(Latitude)) * 
-cos(radians(Longitude) - radians(-74.85114097595216)) + 
-sin(radians('$startphp')) * sin(radians(Latitude)))) 
+$sql = mysqli_query($conn,"SELECT Datemy, ( 6371 * acos(cos(radians('$latphp')) * cos(radians(Latitude)) * 
+cos(radians(Longitude) - radians('$lngphp')) + 
+sin(radians('$latphp')) * sin(radians(Latitude)))) 
 AS distance 
 FROM tabla 
-HAVING distance < 1 ORDER BY distance");
+HAVING distance < 70 ORDER BY distance");
 
 $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
 
