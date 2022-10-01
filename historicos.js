6f68d77ec81619ddf011bf45311dc9373b919e13
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-10-01 11:49:06 -0500

    restored

--- historicos.js
+++ historicos.js
@@ -9,11 +9,11 @@ L.tileLayer(tilesProvider,{
 
 function getInfo(){
     var star= document.getElementById('start').value;
-    var en= document.getElementById('start').value;
+    var en= document.getElementById('end').value;
     var start=star.replace('T',' ');
     var end=en.replace('T',' ');
 
-$.getJSON('post.php', {var1: "start", var2: "start"}, function (data, textStatus, jqXHR) {
+$.getJSON('post.php', {var1: "start", var2: "end"}, function (data, textStatus, jqXHR) {
     console.log(data);
     var lati = data.map(a => a.Latitude);
     var long = data.map(a => a.Longitude);
