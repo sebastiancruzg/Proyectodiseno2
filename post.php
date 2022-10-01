d745d611c54b3532f30994249ae4360034f65115
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-10-01 12:09:28 -0500

    query restored2

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <?php   
 
-$startphp = $_POST['var1'];
-$endphp = $_POST['var2'];
+$startphp = $_GET['var1'];
+$endphp = $_GET['var2'];
 echo $startphp;
 echo $endphp;
 
@@ -17,8 +17,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = mysqli_query($conn,"SELECT Latitude, Longitude FROM tabla  WHERE Datemy >= $startphp AND Datemy 
-<= $endphp ");  
+$sql = mysqli_query($conn,"SELECT Latitude, Longitude FROM tabla  WHERE Datemy >= '$startphp' AND Datemy 
+<= '$endphp' ");  
 $result = mysqli_fetch_all($sql, MYSQLI_ASSOC);
 
 exit(json_encode($result));
