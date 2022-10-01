deeb3bbdc102bc0ec17b22054066edc9a9316c16
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-10-01 11:24:50 -0500

    firts try map click2

--- maptry.js
+++ maptry.js
@@ -1,10 +1,12 @@
 const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
+var Map = L.map('map'). setView([10.494963, -75.131142] , 9);
+
 L.tileLayer(tilesProvider,{
     maxZoom: 18,
     attribution: '© OpenStreetMap'
 }).addTo(Map)
 
-var Map = L.map('map'). setView([10.494963, -75.131142] , 9);
+
 
 
