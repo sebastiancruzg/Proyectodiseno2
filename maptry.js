f9fe78135b47eeaf72f131f462d1b5a35657b99d
Camilodr25 <112908070+Camilodr25@users.noreply.github.com>
2022-10-04 13:18:43 -0500

    Add files via upload

--- maptry.js
+++ maptry.js
@@ -12,6 +12,10 @@ Map.on('click', function(e) {
     
 
     $.getJSON('maphp.php', {var1: lat, var2: lng}, function (data, textStatus, jqXHR) {
+
+        if (data.length == 0) {
+            marker.bindPopup ('Su vehículo no ha pasado por este lugar ' ).on('click'); 
+        }
         
         marker.bindPopup ('Su vehículo pasó por este lugar en las siguinetes fechas: ' + data[0].Datemy + ' , ' + data[10].Datemy + ' , ' + data[20].Datemy + ' , ' + data[30].Datemy + ' , ' + data[40].Datemy ).on('click');  
         console.log(data);
